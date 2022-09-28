8003705cf592c26e2a06bbd7846dfc830b1d27c0
Martha Reisinger <marthareisinger@gmail.com>
2022-09-27 21:34:03 -0500

    implemented the bar graph

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,50 @@
 console.log('this is app.js')
 
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
 function DrawBargraph(sampleId)
 {
     console.log('DrawBargraph(${sampleId})');
+
+    d3.json(url).then(data => {
+        console.log(data);
+
+        let samples = data.samples;
+        let resultArray = samples.filter(s => s.id == sampleId);
+        let result = resultArray[0];
+
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        let yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+        console.log(`yticks = ${yticks}`);
+
+        //create a trace object
+        let barData = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks, 
+            type: 'bar',
+            text: otu_labels.slice(0, 10).reverse(),
+            orientation: 'h'
+        
+
+        };
+        
+        //put the trace object into the array
+        let barArray = [barData];
+        //layout object
+        let barLayout = {
+            title: "Top 10 Bacterial Cultures Found",
+            margin: {t: 30, l: 150}
+
+        }
+
+        //plotly function
+        Plotly.newPlot("bar", barArray, barLayout);
+
+
+    });
 
 }
 
